401e8de9510294bc340839950739b93dda7ebfe0
Dilshi Wanniarachchi <110255212+DilshiWanniarachchi@users.noreply.github.com>
2023-05-25 12:35:48 +0530

    Refactoring products and cart apps

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,5 +1,24 @@
 import faker from 'faker';
 
-const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
+const mount = (el) => {
+    const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
 
-document.querySelector('#dev-cart').innerHTML = cartText;
+    el.innerHTML = cartText;
+};
+
+//Situation #1
+if (process.env.NODE_ENV === 'development') {
+    const el = document.querySelector('#dev-cart');
+
+    if(el) {
+        mount(el);
+    }
+}
+
+//Situation #2
+export { mount };
+
+
+
+
+
